e09db105df70984b48d652dca3d5c156e469c165
Florian Lancon <florian.lancon@gmail.com>
2021-07-13 13:35:19 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/head.inc.html';?>
+</head>
+
+<body>
+    <header>
+        <?php include 'includes/header.inc.html';?>
+    </header>
+
+<div class="row">
+
+        <div class="col-3 p-5">
+            <nav>
+                <button type="button" class="btn btn-outline-secondary btn-block"> Home </button>
+            </nav>
+        </div>
+
+        <div class="col-9 p-5">
+            <section>
+                <button type="button" class="btn btn-primary text-white px-5">Ajouter des données</a>
+            </section>
+        </div>
+
+</div>
+    <footer>
+        <?php include 'includes/footer.inc.html';?>
+    </footer>
+</body>
+</html>
